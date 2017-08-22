530bdf055c4ab9b9768e3be6ca29745db8e40522
ymukhin <yurymukhin97@gmail.com>
2017-08-22 12:06:10 +0300

    aop:aspect

---
+++ src/main/java/IdolCompetition/Contestant.java
@@ -0,0 +1,8 @@
+package IdolCompetition;
+
+/**
+ * Created by ymukhin on 8/22/2017.
+ */
+public interface Contestant {
+    void receiveAward();
+}
---
+++ src/main/java/IdolCompetition/GraciousContestant.java
@@ -0,0 +1,10 @@
+package IdolCompetition;
+
+/**
+ * Created by ymukhin on 8/22/2017.
+ */
+public class GraciousContestant implements Contestant{
+    public void receiveAward() {
+
+    }
+}
