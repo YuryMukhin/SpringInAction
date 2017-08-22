878ba05578dabd98e192a84f45f434eeead6c2ea
ymukhin <yurymukhin97@gmail.com>
2017-08-22 14:12:06 +0300

    creating aspects using annotations @AspectJ

--- src/main/java/IdolCompetition/Audience.java
+++ src/main/java/IdolCompetition/Audience.java
@@ -1,28 +1,38 @@
 package IdolCompetition;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 
 /**
  * Created by ymukhin on 8/17/2017.
  */
+@Aspect
 public class Audience {
+    @Pointcut("execution(* IdolCompetition.Performer.perform())")
+    public void performance(){}
+/*
+    @Before("performance()")
     public void takeSeats(){ // before Performance
         System.out.println("The audience is taking their seats");
     }
 
+    @Before("performance()")
     public void turnOffCellPhones(){// before Performance
         System.out.println("The audience is turning of their cellphones");
     }
 
+    @AfterReturning("performance()")
     public void apppaud(){ //After performance
         System.out.println("Clap clap clap");
     }
 
+    @AfterThrowing("performance()")
     public void demandRefund(){ //After failed performance
         System.out.println("Boo! We want our money back");
     }
-
-   public void watchPerformance(ProceedingJoinPoint joinPoint){
+*/
+    @Around("performance()")
+    public void watchPerformance(ProceedingJoinPoint joinPoint){
         try{
             System.out.println("The audience is taking their seats");
             System.out.println("The audience is turning of their cellphones");
